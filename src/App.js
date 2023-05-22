34955964b1e8e96d7b90e98ffda74a3e2108e12d
zabihhaqqani <mdzabihhaqqani@gmail.com>
2023-05-22 11:11:33 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>e-commerce</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import '../Home/Home.css'
+
+export function Home() {
+  return <>
+  <header>
+    <h1>This is website</h1>
+  </header>
+  </>;
+}
