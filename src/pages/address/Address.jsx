b88f2363a76520939e567444a2d54a339500a021
zabihhaqqani <mdzabihhaqqani@gmail.com>
2023-07-05 08:00:48 +0530

    style: user information

--- src/pages/address/Address.jsx
+++ src/pages/address/Address.jsx
@@ -3,58 +3,28 @@ import { EditAddress } from "./EditAddress";
 import { useState } from "react";
 import { AddressForm } from "./AddressForm";
 import "./address.css";
+import { useNavigate } from "react-router-dom";
 
 export function Address() {
   const { addressData, dispatch } = useProductContext();
   const [addAddress, setAddAddress] = useState(false);
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Address</h1>
-      <div className="address-container-main">
-        {addAddress && <AddressForm setAddAddress={setAddAddress} />}
-        {addressData?.length === 0 && (
-          <h4 style={{ margin: "1rem" }}>No Addresses!</h4>
-        )}
-        {addressData?.map((data) => {
-          const {
-            id,
-            userName,
-            city,
-            state,
-            country,
-            pincode,
-            mobileNumber,
-            isEdit,
-          } = data;
-          return (
-            <div className="user-detail-address" key={id}>
-              <p>
-                <strong>{userName}</strong>
-              </p>
-              <p>{city}</p>
-              <p>{state}</p>
-              <p>{country}</p>
-              <p>{pincode}</p>
-              <p>{mobileNumber}</p>
-              {isEdit && <EditAddress editId={id} />}
-              <button
-                style={{ backgroundColor: "green" }}
-                className="add-to-cart-btn"
-                onClick={() => dispatch({ type: "EDIT_ADDRESS", payload: id })}
-              >
-                Edit
-              </button>
-              <button
-                className="add-to-cart-btn"
-                onClick={() =>
-                  dispatch({ type: "DELETE_ADDRESS", payload: id })
-                }
-              >
-                Delete
-              </button>
-            </div>
-          );
-        })}
+    <div
+      style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
+    >
+      
+      <div style={{marginTop:"1rem"}} className="inner-user-detail-container">
+        <p onClick={() => navigate("/account-details/userdetails")}>
+          Profile Information
+        </p>
+        <p
+          style={{ backgroundColor: "#FC4C51", color: "white" }}
+          onClick={() => navigate("/account-details/userdetails/address")}
+        >
+          Addresses
+        </p>
+        <p onClick={() => navigate("/order-history")}> Order History</p>
       </div>
       <button
         style={{ backgroundColor: "green" }}
@@ -63,6 +33,57 @@ export function Address() {
       >
         Add New Address
       </button>
+      <div className="address-container-all-main">
+        <div className="address-container-main">
+          <div className="all-user-detail-container"></div>
+          {addAddress && <AddressForm setAddAddress={setAddAddress} />}
+          {addressData?.length === 0 && (
+            <h4 style={{ margin: "1rem" }}>No Addresses!</h4>
+          )}
+          {addressData?.map((data) => {
+            const {
+              id,
+              userName,
+              city,
+              state,
+              country,
+              pincode,
+              mobileNumber,
+              isEdit,
+            } = data;
+            return (
+              <div className="user-detail-address" key={id}>
+                <p>
+                  <strong>{userName}</strong>
+                </p>
+                <p>{city}</p>
+                <p>{state}</p>
+                <p>{country}</p>
+                <p>{pincode}</p>
+                <p>{mobileNumber}</p>
+                {isEdit && <EditAddress editId={id} />}
+                <button
+                  style={{ backgroundColor: "green" }}
+                  className="add-to-cart-btn"
+                  onClick={() =>
+                    dispatch({ type: "EDIT_ADDRESS", payload: id })
+                  }
+                >
+                  Edit
+                </button>
+                <button
+                  className="add-to-cart-btn"
+                  onClick={() =>
+                    dispatch({ type: "DELETE_ADDRESS", payload: id })
+                  }
+                >
+                  Delete
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/checkout/orderHistory.jsx
+++ src/pages/checkout/orderHistory.jsx
@@ -1,11 +1,31 @@
+import { useNavigate } from "react-router-dom";
 import { useOrderContext } from "../../contexts/OrderContext";
 import "./orderHistory.css";
 export function OrderHistory() {
   const { orderState } = useOrderContext();
-
+  const navigate = useNavigate();
   return (
-    <div>
-      <h3 style={{ margin: "1rem" }}>Orders Placed:</h3>
+    <div
+      style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
+    >
+      <div
+        style={{ marginTop: "1rem" }}
+        className="inner-user-detail-container"
+      >
+        <p onClick={() => navigate("/account-details/userdetails")}>
+          Profile Information
+        </p>
+        <p onClick={() => navigate("/account-details/userdetails/address")}>
+          Addresses
+        </p>
+        <p
+          style={{ backgroundColor: "#FC4C51", color: "white" }}
+          onClick={() => navigate("/order-history")}
+        >
+          {" "}
+          Order History
+        </p>
+      </div>
       <div className="order-history-container">
         <div>
           {orderState?.orderHistory?.length <= 0 ? (
